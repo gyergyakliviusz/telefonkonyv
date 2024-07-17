9f4e77cf100332e322a7682f648e89a461d8ca1b
Gyergyák Líviusz <89666209+gyergyakliviusz@users.noreply.github.com>
2024-07-17 11:04:51 +0200

    Add files via upload

---
+++ TelefonkonyvWinforms/TelefonkonyvWinforms/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace TelefonkonyvWinforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.lstContacts = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtMotherName = new System.Windows.Forms.TextBox();
+            this.txtFatherName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtMobileNo = new System.Windows.Forms.TextBox();
+            this.txtSex = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.txtCitizenNo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(149, 89);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add New";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(353, 89);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(75, 23);
+            this.btnList.TabIndex = 1;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(554, 89);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(554, 147);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(353, 147);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(149, 147);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(75, 23);
+            this.btnModify.TabIndex = 3;
+            this.btnModify.Text = "Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // lstContacts
+            // 
+            this.lstContacts.FormattingEnabled = true;
+            this.lstContacts.Location = new System.Drawing.Point(514, 213);
+            this.lstContacts.Name = "lstContacts";
+            this.lstContacts.Size = new System.Drawing.Size(235, 186);
+            this.lstContacts.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(324, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Telefonkönyv";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Father\'s Name:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(47, 352);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Mother\'s Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(270, 352);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Citizen Number:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(270, 316);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "E-mail:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(270, 281);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Sex:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(270, 245);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Mobile Number:";
+            // 
+            // txtMotherName
+            // 
+            this.txtMotherName.Enabled = false;
+            this.txtMotherName.Location = new System.Drawing.Point(132, 349);
+            this.txtMotherName.Name = "txtMotherName";
+            this.txtMotherName.Size = new System.Drawing.Size(100, 20);
+            this.txtMotherName.TabIndex = 16;
+            // 
+            // txtFatherName
+            // 
+            this.txtFatherName.Enabled = false;
+            this.txtFatherName.Location = new System.Drawing.Point(132, 313);
+            this.txtFatherName.Name = "txtFatherName";
+            this.txtFatherName.Size = new System.Drawing.Size(100, 20);
+            this.txtFatherName.TabIndex = 17;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Enabled = false;
+            this.txtAddress.Location = new System.Drawing.Point(132, 278);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 18;
+            // 
+            // txtName
+            // 
+            this.txtName.Enabled = false;
+            this.txtName.Location = new System.Drawing.Point(132, 242);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 19;
+            // 
+            // txtMobileNo
+            // 
+            this.txtMobileNo.Enabled = false;
+            this.txtMobileNo.Location = new System.Drawing.Point(357, 242);
+            this.txtMobileNo.Name = "txtMobileNo";
+            this.txtMobileNo.Size = new System.Drawing.Size(100, 20);
+            this.txtMobileNo.TabIndex = 23;
+            // 
+            // txtSex
+            // 
+            this.txtSex.Enabled = false;
+            this.txtSex.Location = new System.Drawing.Point(357, 278);
+            this.txtSex.Name = "txtSex";
+            this.txtSex.Size = new System.Drawing.Size(100, 20);
+            this.txtSex.TabIndex = 22;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Enabled = false;
+            this.txtMail.Location = new System.Drawing.Point(357, 313);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(100, 20);
+            this.txtMail.TabIndex = 21;
+            // 
+            // txtCitizenNo
+            // 
+            this.txtCitizenNo.Enabled = false;
+            this.txtCitizenNo.Location = new System.Drawing.Point(357, 349);
+            this.txtCitizenNo.Name = "txtCitizenNo";
+            this.txtCitizenNo.Size = new System.Drawing.Size(100, 20);
+            this.txtCitizenNo.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMobileNo);
+            this.Controls.Add(this.txtSex);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.txtCitizenNo);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtFatherName);
+            this.Controls.Add(this.txtMotherName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstContacts);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.btnAdd);
+            this.Enabled = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.ListBox lstContacts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtMotherName;
+        private System.Windows.Forms.TextBox txtFatherName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtMobileNo;
+        private System.Windows.Forms.TextBox txtSex;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.TextBox txtCitizenNo;
+    }
+}
+
---
+++ TelefonkonyvWinforms/TelefonkonyvWinforms/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace TelefonkonyvWinforms
+{
+    public partial class Form1 : Form
+    {
+        private List<Contact> contacts;
+
+        public Form1()
+        {
+            InitializeComponent();
+            contacts = new List<Contact>();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var contact = new Contact
+            {
+                Name = txtName.Text,
+                Address = txtAddress.Text,
+                FatherName = txtFatherName.Text,
+                MotherName = txtMotherName.Text,
+                MobileNo = long.Parse(txtMobileNo.Text),
+                Sex = txtSex.Text,
+                Mail = txtMail.Text,
+                CitizenNo = txtCitizenNo.Text
+            };
+
+            contacts.Add(contact);
+            MessageBox.Show("Contact added successfully!");
+
+            ClearTextFields();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            lstContacts.Items.Clear();
+            foreach (var contact in contacts)
+            {
+                lstContacts.Items.Add(contact);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            var name = txtName.Text;
+            var contact = contacts.FirstOrDefault(c => c.Name == name);
+            if (contact != null)
+            {
+                contact.Address = txtAddress.Text;
+                contact.FatherName = txtFatherName.Text;
+                contact.MotherName = txtMotherName.Text;
+                contact.MobileNo = long.Parse(txtMobileNo.Text);
+                contact.Sex = txtSex.Text;
+                contact.Mail = txtMail.Text;
+                contact.CitizenNo = txtCitizenNo.Text;
+                MessageBox.Show("Contact modified successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Contact not found!");
+            }
+
+            ClearTextFields();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var name = txtName.Text;
+            var contact = contacts.FirstOrDefault(c => c.Name == name);
+            if (contact != null)
+            {
+                txtAddress.Text = contact.Address;
+                txtFatherName.Text = contact.FatherName;
+                txtMotherName.Text = contact.MotherName;
+                txtMobileNo.Text = contact.MobileNo.ToString();
+                txtSex.Text = contact.Sex;
+                txtMail.Text = contact.Mail;
+                txtCitizenNo.Text = contact.CitizenNo;
+                MessageBox.Show("Contact found!");
+            }
+            else
+            {
+                MessageBox.Show("Contact not found!");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var name = txtName.Text;
+            var contact = contacts.FirstOrDefault(c => c.Name == name);
+            if (contact != null)
+            {
+                contacts.Remove(contact);
+                MessageBox.Show("Contact deleted successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Contact not found!");
+            }
+
+            ClearTextFields();
+        }
+
+        private void ClearTextFields()
+        {
+            txtName.Clear();
+            txtAddress.Clear();
+            txtFatherName.Clear();
+            txtMotherName.Clear();
+            txtMobileNo.Clear();
+            txtSex.Clear();
+            txtMail.Clear();
+            txtCitizenNo.Clear();
+        }
+    }
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public long MobileNo { get; set; }
+        public string Sex { get; set; }
+        public string Mail { get; set; }
+        public string CitizenNo { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {MobileNo}";
+        }
+    }
+
+}
